e6fcf7372f645623ab7bdc64b86e232a08e5ac59
Adson Mozart <mozar7.dev@gmail.com>
2026-08-15 12:22:33 -0300

    chore: primeiro commit, adicionando o escopo do backend em Java já com as bibliotecas que planejo utilizar

---
+++ backend/src/main/java/br/com/mozart/bilheteria_digital/BilheteriaDigitalApplication.java
@@ -0,0 +1,13 @@
+package br.com.mozart.bilheteria_digital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilheteriaDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilheteriaDigitalApplication.class, args);
+	}
+
+}
