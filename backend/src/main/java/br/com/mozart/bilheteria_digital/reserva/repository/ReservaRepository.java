6e46cd8053c4f456dac23a9712ca6324a4c2005f
Adson Mozart <mozar7.dev@gmail.com>
2026-08-16 16:54:11 -0300

    feat: adicionando repositório do domínio Assento e adicionando também as annotations dos demais repositórios

--- backend/src/main/java/br/com/mozart/bilheteria_digital/reserva/repository/ReservaRepository.java
+++ backend/src/main/java/br/com/mozart/bilheteria_digital/reserva/repository/ReservaRepository.java
@@ -3,9 +3,11 @@ package br.com.mozart.bilheteria_digital.reserva.repository;
 import br.com.mozart.bilheteria_digital.reserva.domain.Reserva;
 import br.com.mozart.bilheteria_digital.reserva.domain.StatusReserva;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ReservaRepository extends JpaRepository<Reserva, Long> {
 
     List<Reserva> findByClienteId(Long clienteId);
