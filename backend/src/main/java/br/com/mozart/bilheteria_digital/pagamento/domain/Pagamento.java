628f0015c9b1f26d315968c5e74da033f518709a
Adson Mozart <mozar7.dev@gmail.com>
2026-08-16 20:05:15 -0300

    feat: adicionando domínio Pagamento, seus Enums com os status categorizados e seu repositório

---
+++ backend/src/main/java/br/com/mozart/bilheteria_digital/pagamento/domain/Pagamento.java
@@ -0,0 +1,88 @@
+package br.com.mozart.bilheteria_digital.pagamento.domain;
+
+import br.com.mozart.bilheteria_digital.reserva.domain.Reserva;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "pagamentos")
+public class Pagamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "reserva_id", nullable = false, unique = true)
+    private Reserva reserva;
+
+    @Column(name = "pagamento_stripe_id", length = 120)
+    private String pagamentoStripeId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private StatusPagamento status = StatusPagamento.PENDENTE;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal valor;
+
+    @Column(name = "data_criacao", nullable = false, insertable = false, updatable = false)
+    private LocalDateTime dataCriacao;
+
+    // construtores
+    protected Pagamento() {
+
+    }
+
+    public Pagamento(Reserva reserva, BigDecimal valor) {
+        this.reserva = reserva;
+        this.valor = valor;
+    }
+
+
+    // metodo de vinculação do ID recebido pelo stripe
+    public void vincularPagamentoStripe(String pagamentoStripeId) {
+        this.pagamentoStripeId = pagamentoStripeId;
+    }
+
+    // metodo de comportamentos do pagamento
+    public void aprovar() {
+        this.status = StatusPagamento.APROVADO;
+    }
+
+    public void recusar() {
+        this.status = StatusPagamento.RECUSADO;
+    }
+
+    // metodo de comparacao para validar aprovacao do pagamento
+    public boolean estaAprovado() {
+        return this.status == StatusPagamento.APROVADO;
+    }
+
+    // gets
+    public Long getId() {
+        return id;
+    }
+
+    public Reserva getReserva() {
+        return reserva;
+    }
+
+    public String getPagamentoStripeId() {
+        return pagamentoStripeId;
+    }
+
+    public StatusPagamento getStatus() {
+        return status;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+}
---
+++ backend/src/main/java/br/com/mozart/bilheteria_digital/pagamento/domain/StatusPagamento.java
@@ -0,0 +1,7 @@
+package br.com.mozart.bilheteria_digital.pagamento.domain;
+
+public enum StatusPagamento {
+    PENDENTE,
+    APROVADO,
+    RECUSADO
+}
