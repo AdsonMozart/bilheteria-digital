542f8f5f55c9dfa7cc459f71b7ca1bd0857309e7
Adson Mozart <mozar7.dev@gmail.com>
2026-08-16 20:05:15 -0300

    feat: adicionando domínio Pagamento, seus Enums com os status categorizados e seu repositório

---
+++ backend/src/main/java/br/com/mozart/bilheteria_digital/pagamento/repository/PagamentoRepository.java
@@ -0,0 +1,20 @@
+package br.com.mozart.bilheteria_digital.pagamento.repository;
+
+import br.com.mozart.bilheteria_digital.pagamento.domain.Pagamento;
+import br.com.mozart.bilheteria_digital.pagamento.domain.StatusPagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
+
+    Optional<Pagamento> findByReservaId(Long reservaId);
+
+    Optional<Pagamento> findByPagamentoStripeId(String pagamentoStripeId);
+
+    List<Pagamento> findByStatus(StatusPagamento status);
+
+}
