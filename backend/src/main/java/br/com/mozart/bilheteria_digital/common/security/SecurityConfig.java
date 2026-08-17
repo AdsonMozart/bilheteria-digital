18e9abb97c8eb37d1c54b404ab65bff5fa64a210
Adson Mozart <mozar7.dev@gmail.com>
2026-08-17 17:11:13 -0300

    feat: adicionando filtro de autenticação de usuario JWT

--- backend/src/main/java/br/com/mozart/bilheteria_digital/common/security/SecurityConfig.java
+++ backend/src/main/java/br/com/mozart/bilheteria_digital/common/security/SecurityConfig.java
@@ -9,9 +9,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -22,6 +29,7 @@ public class SecurityConfig {
                         requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
