5afb5052138c035f828e53e57879ae309f0e5050
Adson Mozart <mozar7.dev@gmail.com>
2026-08-17 15:31:29 -0300

    feat: criando controller para disponibilizar os endpoints de registro e login

---
+++ backend/src/main/java/br/com/mozart/bilheteria_digital/auth/controller/AuthController.java
@@ -0,0 +1,34 @@
+package br.com.mozart.bilheteria_digital.auth.controller;
+
+import br.com.mozart.bilheteria_digital.auth.dto.AuthResponse;
+import br.com.mozart.bilheteria_digital.auth.dto.LoginRequest;
+import br.com.mozart.bilheteria_digital.auth.dto.RegistroRequest;
+import br.com.mozart.bilheteria_digital.auth.service.AuthService;
+import br.com.mozart.bilheteria_digital.usuario.domain.Usuario;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/registrar")
+    public ResponseEntity<AuthResponse> registrar(@RequestBody RegistroRequest request) {
+        return ResponseEntity.ok(authService.registrar(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
